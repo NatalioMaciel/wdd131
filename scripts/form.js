3f525215ab5458e4d5357fef6ffa639afa5d9508
NatalioMaciel <mac24026@byui.edu>
2024-10-26 00:00:37 -0300

    .

--- scripts/form.js
+++ scripts/form.js
@@ -1,52 +1,38 @@
 const products = [
     {
-      id: "fc-1888",
-      name: "flux capacitor",
-      averagerating: 4.5
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
     },
     {
-      id: "fc-2050",
-      name: "power laces",
-      averagerating: 4.7
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
     },
     {
-      id: "fs-1987",
-      name: "time circuits",
-      averagerating: 3.5
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
     },
     {
-      id: "ac-2000",
-      name: "low voltage reactor",
-      averagerating: 3.9
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
     },
     {
-      id: "jj-1969",
-      name: "warp equalizer",
-      averagerating: 5.0
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
     }
 ];
-
 const productList = document.getElementById("productReview")
 
 products.forEach(product => {
-    productList.innerHTML +=`
+    productList.innerHTML += `
         <select name="product" id="productReview" required>
             <option id="${product.id}" name="${product.name}">${product.name}</option>
         </select>
     `
-});
-
-let reviewCounter = localStorage.getItem("reviewCounter") || 0;
-document.getElementById("reviewCounter").textContent = reviewCounter;
-
-const reviewForm = document.getElementById("reviewForm");
-reviewForm.addEventListener("submit", (event) => {
-  event.preventDefault(); 
-  reviewCounter++;
-  localStorage.setItem("reviewCounter", reviewCounter);
-  document.getElementById("reviewCounter").textContent = reviewCounter;
-
-  window.location.href = "review.html";
 });
 
 function displayCopyright() {
@@ -59,7 +45,20 @@ function displayLastModified() {
     document.getElementById('last-modified').textContent = `Last modification: ${lastModified}`;
 };
 
-window.onload = 
-displayCopyright(),
-displayLastModified()
-;
+const visitsReview = document.querySelector(".reviewCounter")
+let numReviews = Number(window.localStorage.getItem("numReviews")) || 0;
+
+reviewForm.addEventListener("submit", (event) => {
+    if (numReviews !== 0) {
+        visitsReview.textContent = numReviews;
+    } else {
+        visitsReview.textContent = `1`;
+    }
+    numReviews++;
+    localStorage.setItem("numReviews", numReviews);
+});
+
+window.onload =
+    displayCopyright(),
+    displayLastModified()
+    ;
