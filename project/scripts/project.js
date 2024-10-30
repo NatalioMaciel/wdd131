8deee477a43c7e180c1e8850ee4807aad9ddc36c
NatalioMaciel <mac24026@byui.edu>
2024-10-30 01:54:24 -0300

    ...

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,7 +1,6 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambutton.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambutton.classList.toggle('show');
@@ -138,11 +137,6 @@ homelink.addEventListener("click", () => {
         tHome = temples.templeName;
         return tHome !== "";
     });
-
-    createTempleCard(homeTemples);
-}); */
-
-
 
 function displayCopyright() {
     const currentYear = new Date().getFullYear();
